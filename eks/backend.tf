4cb2958b91dcda9f51e7a3234d9ae7d7d0748cdb
madhur431997 <maddhur.v28@gmail.com>
2024-08-23 13:15:32 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,9 +9,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-bucket14253436"
     region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
   }
 }
 
